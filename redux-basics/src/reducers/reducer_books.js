d88cf382b08b8eaea351b181c31cdcd0e55ae152
Steven Roper <sroper@uheaa.ushe.local>
2016-12-01 11:32:19 -0700

    Add first container and reducer.

---
+++ redux-basics/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Learn Things' },
+    { title: 'The Way of Kings' },
+    { title: 'Jack Reacher' },
+    { title: 'Learn More Things' },
+  ]
+}

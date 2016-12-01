676ba08a5e957ddb1f1e7276e952cf0a4cf124c8
Steven Roper <sroper@uheaa.ushe.local>
2016-12-01 14:25:32 -0700

    Initial commit and creating search bar.

---
+++ weather-forecast/src/containers/search_bar.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({ term: e.target.value });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          placeholder="Search for a city's fix-day forecast"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange } />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Search</button>
+        </span>
+      </form>
+    );
+  }
+} 

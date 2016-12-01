22a9b30b93ec3d381aad96da55c34c541280237c
Steven Roper <sroper@uheaa.ushe.local>
2016-12-01 08:30:32 -0700

    Initial commit plus some work

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyCQheULrA0hkX1S4Iw7w-oK6jTjfP9Qr14';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'puppies'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
